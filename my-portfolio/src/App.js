35d29e53f24a6cd12f9792710399fce37170d3d1
ejezie <shady07chinex@gmail.com>
2021-05-13 23:06:23 +0100

    fix sass

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,13 +1,20 @@
 import "./App.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <div className="sidebar"></div>
-        <div className="main__body"></div>
-        <main>hello</main>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="main__body">
+          <h1>MAIN BODY</h1>
+        </div>
+        <main>
+          <Switch>{/* <Route exact path="/" component={Sidebar}/> */}</Switch>
+        </main>
       </div>
     </Router>
   );
---
+++ my-portfolio/src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import mypic from "../images/Chinedu.jpg";
+import { Link } from "react-router-dom";
+
+function Sidebar() {
+  return (
+    <div>
+      <div className="profile">
+        <img src={mypic} alt="chinedu" />
+      </div>
+      <div className="side__links">
+        <ul className="list__links">
+          <li className="link__item">
+            <Link></Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
