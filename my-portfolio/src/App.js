4f9738fcd77d749e5c62e2ea8f950036b059948d
ejezie <shady07chinex@gmail.com>
2021-05-14 01:59:12 +0100

    styled sidebar

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <div className="sidebar">
+        <div className="sidebarw">
           <Sidebar />
         </div>
         <div className="main__body">
-        <div className="fade"></div>
+        {/* <div className="fade"></div> */}
         </div>
         <main>
-          <Switch>{/* <Route exact path="/" component={Sidebar}/> */}</Switch>
+          {/* <Switch><Route exact path="/" component={}/></Switch> */}
         </main>
       </div>
     </Router>
--- my-portfolio/src/components/Sidebar.jsx
+++ my-portfolio/src/components/Sidebar.jsx
@@ -1,21 +1,49 @@
 import React from "react";
 import mypic from "../images/Chinedu.jpg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="profile">
-        <img src={mypic} alt="chinedu" />
+      <div className="side__wrap">
+        <div className="profile">
+          <img src={mypic} alt="chinedu" />
+        </div>
+        <div className="side__links">
+          <ul className="list__links">
+            <li className="link__item">
+              <NavLink to="/" exact activeClassName="active">
+                Home
+              </NavLink>
+            </li>
+            <li className="link__item">
+              <NavLink to="/about" exact activeClassName="active">
+                About
+              </NavLink>
+            </li>
+            <li className="link__item">
+              <NavLink to="/portfolio" exact activeClassName="active">
+                Portfolio
+              </NavLink>
+            </li>
+            <li className="link__item">
+              <NavLink to="/blogs" exact activeClassName="active">
+                Blogs
+              </NavLink>
+            </li>
+            <li className="link__item">
+              <NavLink to="/contact" exact activeClassName="active">
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <footer className="footer">
+          <i class="far fa-copyright"></i>
+          <p>copy right reserved 2021</p>
+        </footer>
       </div>
-      <div className="side__links">
-        <ul className="list__links">
-          <li className="link__item">
-            <Link></Link>
-          </li>
-        </ul>
-      </div>
-    </div>  
+    </div>
   );
 }
 
