8824812c488315c4f71071231059775a9cc81b5f
ejezie <shady07chinex@gmail.com>
2021-05-17 00:13:47 +0100

    bottom menu toggle effect

--- my-portfolio/src/pages/NavBottom.jsx
+++ my-portfolio/src/pages/NavBottom.jsx
@@ -1,50 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBottom() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  let navMenuClass = ["nav__menu"];
+  if (navToggle) {
+    navMenuClass.push("shownavmenu");
+  }
+  const toggleNavMenu = () => {
+    setNavToggle(!navToggle);
+  };
   return (
     <div className="navbottom">
       <nav className="bottom__navwrap">
         <NavLink to="/" className="nav__name">
           Ejezie
         </NavLink>
-        <div className="nav__menu">
+        <div className={navMenuClass.join(" ")}>
           <ul className="nav__gridlist">
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-home"></i><p>Home</p> 
+              <NavLink
+                className="nav__link"
+                onClick={toggleNavMenu}
+                onClick={toggleNavMenu}
+                to="/">
+                <i class="far fa-home"></i>
+                <p>Home</p>
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-user"></i><p>About</p> 
+              <NavLink
+                className="nav__link"
+                onClick={toggleNavMenu}
+                onClick={toggleNavMenu}
+                to="/about">
+                <i class="far fa-user"></i>
+                <p>About</p>
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-file-alt"></i><p>Skills</p>
+              <NavLink className="nav__link" onClick={toggleNavMenu} to="/">
+                <i class="far fa-file-alt"></i>
+                <p>Skills</p>
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-briefcase"></i><p>Services</p>
+              <NavLink className="nav__link" onClick={toggleNavMenu} to="/">
+                <i class="far fa-briefcase"></i>
+                <p>Services</p>
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-image"></i><p>Portfolio</p>
+              <NavLink className="nav__link" onClick={toggleNavMenu} to="/">
+                <i class="far fa-image"></i>
+                <p>Portfolio</p>
               </NavLink>
             </li>
             <li className="nav__item">
-              <NavLink className="nav__link" to="/">
-                <i class="far fa-envelope-open-text"></i><p>Contactme</p>
+              <NavLink className="nav__link" onClick={toggleNavMenu} to="/">
+                <i class="far fa-envelope-open-text"></i>
+                <p>Contactme</p>
               </NavLink>
             </li>
-            <i class="fas fa-times nav__close"></i>
           </ul>
+          <i class="fas fa-times nav__close" onClick={toggleNavMenu}></i>
         </div>
         <div className="nav__buttons">
-          <div className="nav__toggle">
+          <div className="nav__toggle" onClick={toggleNavMenu}>
             <i class="fas fa-th-large"></i>
           </div>
         </div>
