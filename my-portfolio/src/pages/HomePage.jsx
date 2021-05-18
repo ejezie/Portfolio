adaa031d08f176c8f63d6622f24d2fc444cab49f
ejezie <shady07chinex@gmail.com>
2021-05-18 23:27:41 +0100

    styled home page mobile view

--- my-portfolio/src/pages/HomePage.jsx
+++ my-portfolio/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CanvasText from "../components/CanvasText";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom";
 import me from "../images/me.png";
 
 function HomePage() {
@@ -14,12 +14,17 @@ function HomePage() {
           </div>
           <div className="home__text">
             <h2>Hi, I AM EJEZIE </h2>
-            <p>
+            <p className="describe__p">
               An enthusiatic frontend developer with good eye for simple elegant
               user friendly designs
             </p>
-            <NavLink to="/contacts">
-              Contact me
+            <NavLink to="/contacts" className="contact">
+              Contact me<i class="fal fa-paper-plane send"></i>
+            </NavLink>
+            <NavLink to="/" className="scroll">
+              <i class="fas fa-mouse mouse"></i>
+              <p className="scroll__p">scroll</p>
+              <i class="fas fa-long-arrow-alt-down arrow"></i>
             </NavLink>
           </div>
         </div>
